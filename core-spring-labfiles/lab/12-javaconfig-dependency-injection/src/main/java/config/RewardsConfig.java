e909065968daf73e47e4971cfc0e375dd6c3ec08
Marko Zaninović <mzaninovic555@gmail.com>
2025-12-11 14:38:49 +0100

    12-javaconfig-dependency-injection solved

--- core-spring-labfiles/lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
+++ core-spring-labfiles/lab/12-javaconfig-dependency-injection/src/main/java/config/RewardsConfig.java
@@ -1,5 +1,16 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import rewards.RewardNetwork;
+import rewards.internal.RewardNetworkImpl;
+import rewards.internal.account.AccountRepository;
+import rewards.internal.account.JdbcAccountRepository;
+import rewards.internal.restaurant.JdbcRestaurantRepository;
+import rewards.internal.restaurant.RestaurantRepository;
+import rewards.internal.reward.JdbcRewardRepository;
+import rewards.internal.reward.RewardRepository;
+
 import javax.sql.DataSource;
 
 /**
@@ -10,29 +21,29 @@ import javax.sql.DataSource;
  * - Injecting dependencies through constructor injection
  * - Creating Spring application context in the test code
  *   (WITHOUT using Spring testContext framework)
- *
- * TODO-01: Make this class a Spring configuration class
+ * <p>
+ * TODO-01: Make this class a Spring configuration class - done
  * - Use an appropriate annotation.
- *
+ * <p>
  * TODO-02: Define four empty @Bean methods, one for the
- *          reward-network and three for the repositories.
+ *          reward-network and three for the repositories. - done
  * - The names of the beans should be:
  *   - rewardNetwork
  *   - accountRepository
  *   - restaurantRepository
  *   - rewardRepository
- *
- * TODO-03: Inject DataSource through constructor injection
+ * <p>
+ * TODO-03: Inject DataSource through constructor injection - done
  * - Each repository implementation has a DataSource
  *   property to be set, but the DataSource is defined
  *   elsewhere (TestInfrastructureConfig.java), so you
  *   will need to define a constructor for this class
  *   that accepts a DataSource parameter.
  * - As it is the only constructor, @Autowired is optional.
- *
+ * <p>
  * TODO-04: Implement each @Bean method to contain the code
  *          needed to instantiate its object and set its
- *          dependencies
+ *          dependencies - done
  * - You can create beans from the following implementation classes
  *   - rewardNetwork bean from RewardNetworkImpl class
  *   - accountRepository bean from JdbcAccountRepository class
@@ -42,9 +53,41 @@ import javax.sql.DataSource;
  *   not an implementation.
  */
 
+@Configuration
 public class RewardsConfig {
 
-	// Set this by adding a constructor.
-	private DataSource dataSource;
+    // Set this by adding a constructor.
+    private DataSource dataSource;
 
+    public RewardsConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public RewardNetwork rewardNetwork() {
+        return new RewardNetworkImpl(
+                accountRepository(), restaurantRepository(), rewardRepository()
+        );
+    }
+
+    @Bean
+    public AccountRepository accountRepository() {
+        JdbcAccountRepository repository = new JdbcAccountRepository();
+        repository.setDataSource(dataSource);
+        return repository;
+    }
+
+    @Bean
+    public RestaurantRepository restaurantRepository() {
+        JdbcRestaurantRepository repository = new JdbcRestaurantRepository();
+        repository.setDataSource(dataSource);
+        return repository;
+    }
+
+    @Bean
+    public RewardRepository rewardRepository() {
+        JdbcRewardRepository repository = new JdbcRewardRepository();
+        repository.setDataSource(dataSource);
+        return repository;
+    }
 }
